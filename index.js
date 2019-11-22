52a3e4b6bd9caec05c348d6261b3ce7f13ad587e
Alexandru Branza <branza.alex@gmail.com>
2019-11-22 08:19:48 +0200

    Add skipping / genres / improved meta info

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+const qs = require('querystring')
 app.use(cors())
-
 const manifest = {
 	id: 'org.imdblist',
-	version: '0.0.1',
+	version: '0.0.2',
 	name: 'IMDB List Add-on',
 	description: 'Add-on to create a catalog from IMDB lists.',
 	resources: ['catalog'],
@@ -16,28 +14,50 @@ const manifest = {
 		{
 			id: 'imdb-movie-list',
 			name: 'IMDB Movie List',
-			type: 'movie'
+			type: 'movie',
+			genres: ["Action", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", "Film Noir", "History", "Horror", "Music", "Musical", "Mystery", "Romance", "Sci-Fi", "Short Film", "Sport", "Superhero", "Thriller", "War", "Western"],
+			extra: [{ name: 'skip' },{ name: 'genre' }]
 		}, {
 			id: 'imdb-series-list',
 			name: 'IMDB Series List',
-			type: 'series'
+			type: 'series',
+			genres: ["Action", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", "Film Noir", "History", "Horror", "Music", "Musical", "Mystery", "Romance", "Sci-Fi", "Short Film", "Sport", "Superhero", "Thriller", "War", "Western"],
+			extra: [{ name: 'skip' },{ name: 'genre' }]
 		}
 	]
 }
-
-app.get('/:listId/manifest.json', (req, res) => {
-	res.setHeader('Cache-Control', 'max-age=604800') // one week
-	res.setHeader('Content-Type', 'application/json')
-    res.send(manifest)
+const listManifest = {}
+app.get('/:listId/:sort?/manifest.json', (req, res) => {
+	const cacheTag = req.params.listId + '[]' + (req.params.sort || 'list_order')
+	function respond(msg) {
+		res.setHeader('Cache-Control', 'max-age=86400, public') // one day
+		res.setHeader('Content-Type', 'application/json')
+		res.send(msg)
+	}
+	function tryRespond() {
+		if (listManifest[cacheTag]) {
+			respond(listManifest[cacheTag])
+			return true
+		} else
+			return false
+	}
+	const responded = tryRespond()
+	if (!responded) {
+		queue.push({ id: cacheTag }, (err, done) => {
+			if (done) {
+				const tryAgain = tryRespond()
+				if (tryAgain)
+					return
+			}
+			respond(manifest)
+		})
+	}
 })
-
 const needle = require('needle')
-
 const headers = {
 	'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; TA-1053 Build/OPR1.170623.026) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3368.0 Mobile Safari/537.36',
 	'Accept-Language': 'en-US,en;q=0.8',
 }
-
 function imageResize(posterUrl, width) {
 	if (!posterUrl) return null
 	if (!posterUrl.includes('amazon.com') && !posterUrl.includes('imdb.com')) return posterUrl
@@ -48,37 +68,92 @@ function imageResize(posterUrl, width) {
 	}
 	return posterUrl
 }
-
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 function toMeta(obj) {
 	const titleYear = obj.primary.year && obj.primary.year[0] ? obj.primary.year.length > 1 ? ' (' + obj.primary.year[0] + '-' + obj.primary.year[1] + ')' : ' (' + obj.primary.year[0] + ')' : ''
+	let description = ''
+	if ((obj.ratings || {}).rating) {
+		if ((obj.ratings || {}).votes)
+			description += 'IMDB Rating: ' + obj.ratings.rating + '/10 (' + numberWithCommas(obj.ratings.votes) + ')'
+		if ((obj.ratings || {}).metascore)
+			description += '\n' + 'Metascore: ' + obj.ratings.metascore + '%'
+	}
+	if (obj.plot) {
+		if (description)
+			description += '\n\n'
+		description += obj.plot
+	}
 	return {
 		id: obj.id || null,
 		name: obj.primary && obj.primary.title ? obj.primary.title + (titleYear || '') : null,
 		poster: obj.poster && obj.poster.url ? imageResize(obj.poster.url, 250) : null,
-		type: obj.type == 'featureFilm' ? 'movie' : 'series'
+		type: obj.type == 'featureFilm' ? 'movie' : 'series',
+		genres: (obj.metadata || {}).genres || [],
+		description
 	}
 }
+const sorts = {
+	'list_order': 'list_order%2Casc',
+	'popularity': 'moviemeter%2Casc',
+	'alphabetical': 'alpha%2Casc',
+	'rating': 'user_rating%2Cdesc',
+	'votes': 'num_votes%2Cdesc',
+	'released': 'release_date%2Cdesc',
+	'date_added': 'date_added%2Cdesc'
+}
+const sortsTitle = {
+	'list_order': ' by List Order',
+	'popularity': ' by Popularity',
+	'alphabetical': ' by Alphabetical',
+	'rating': ' by Rating',
+	'votes': ' by Nr Votes',
+	'released': ' by Release Date',
+	'date_added': ' by Date Added'
+}
 
-function getList(listId, cb) {
+const namedQueue = require('named-queue')
+const queue = new namedQueue((task, cb) => {
+	const listId = task.id.split('[]')[0]
+	const sort = task.id.split('[]')[1]
+	const skip = task.id.split('[]')[2]
+	const page = Math.floor(parseInt(skip) / 50) +1
+	const genre = task.id.split('[]')[3]
+	let type = task.id.split('[]')[4]
+
+	if (type == 'series')
+		type = 'tvSeries'
+
 	if (listId) {
 		headers.referer = 'https://m.imdb.com/list/'+listId+'/'
-		const getUrl = 'https://m.imdb.com/list/'+listId+'/search?sort=date_added%2Cdesc&view=grid&tracking_tag=&pageId='+listId+'&pageType=list'
+		const getUrl = 'https://m.imdb.com/list/'+listId+'/search?sort='+sorts[sort]+'&view=grid&tracking_tag=&title_type=' + type + '&pageId='+listId+'&pageType=list&page='+page+(genre ? '&genres=' + genre : '')
 		needle.get(getUrl, { headers }, (err, resp) => {
 			if (!err && resp && resp.body) {
+				const cacheTag = task.id
 				const jObj = resp.body
 				if (jObj.titles && Object.keys(jObj.titles).length) {
-					manifest.types.forEach(el => { cache[el][listId] = [] })
+					const metas = []
 					for (let key in jObj.titles) {
 						const el = jObj.titles[key]
 						const metaType = el.type == 'featureFilm' ? 'movie' : el.type == 'series' ? 'series' : null
-						if (metaType) {
-							cache[metaType][listId].push(toMeta(el))
-						}
+						if (metaType)
+							metas.push(toMeta(el))
 					}
+					if (jObj.list && jObj.list.name) {
+						const cloneManifest = JSON.parse(JSON.stringify(manifest))
+						cloneManifest.id = 'org.imdblist' + cacheTag
+						cloneManifest.name = jObj.list.name + sortsTitle[sort]
+						cloneManifest.catalogs.forEach((cat, ij) => {
+							cloneManifest.catalogs[ij].name = jObj.list.name + sortsTitle[sort]
+						})
+						listManifest[cacheTag] = cloneManifest
+					}
+					cb(false, metas)
+					cache[cacheTag] = metas
 					setTimeout(() => {
-						manifest.types.forEach(el => { cache[el][listId] = [] })
+						cache[cacheTag] = []
 					}, 86400000)
-					cb(false, true)
 				} else 
 					cb('Parsing error on ajax call')
 			} else
@@ -86,17 +161,12 @@ function getList(listId, cb) {
 		})
 	} else
 		cb('No list id')
-}
-
-const namedQueue = require('named-queue')
-
-const queue = new namedQueue((task, cb) => {
-	getList(task.id, cb)
 }, Infinity)
-
-const cache = { movie: {}, series: {} }
-
-app.get('/:listId/catalog/:type/:id.json', (req, res) => {
+const cache = {}
+app.get('/:listId/:sort?/catalog/:type/:id/:extra?.json', (req, res) => {
+	const extra = req.params.extra ? qs.parse(req.url.split('/').pop().slice(0, -5)) : {}
+	const skip = extra.skip ? parseInt(extra.skip) : 0
+	const cacheTag = req.params.listId + '[]' + (req.params.sort || 'list_order') + '[]' + skip + '[]' + (extra.genre || '') + '[]' + req.params.type
 	function fail(err) {
 		console.error(err)
 		res.writeHead(500)
@@ -108,27 +178,19 @@ app.get('/:listId/catalog/:type/:id.json', (req, res) => {
 		res.send(msg)
 	}
 	function fetch() {
-		queue.push({ id: req.params.listId }, (err, done) => {
-			if (done) {
-				const userData = cache[req.params.type][req.params.listId]
-				respond(JSON.stringify({ metas: userData }))
+		queue.push({ id: cacheTag }, (err, metas) => {
+			if (metas) {
+				respond(JSON.stringify({ metas }))
 			} else 
 				fail(err || 'Could not get list items')
 		})
 	}
 	if (req.params.listId && ['movie','series'].indexOf(req.params.type) > -1) {
-		if (cache[req.params.type][req.params.listId]) {
-			const userData = cache[req.params.type][req.params.listId]
-			if (userData.length)
-				respond(JSON.stringify({ metas: userData }))
-			else
-				fetch()
-		} else
+		if ((cache[cacheTag] || []).length)
+			respond(JSON.stringify({ metas: cache[cacheTag] }))
+		else
 			fetch()
 	} else
 		fail('Unknown request parameters')
 })
-
-app.listen(7515, () => {
-    console.log('http://127.0.0.1:7515/[imdb-list-id]/manifest.json')
-})
+module.exports = app
